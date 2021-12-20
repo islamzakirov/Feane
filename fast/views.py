25c748f2755bfdf5d6457eb71a017768a81ccf16
islanzakhirov <islanzakhirov>
2021-12-20 19:20:51 +0500

    stable version: 2.0

--- fast/views.py
+++ fast/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from store.models import Product
 
 def home(request):
-    products = Product.objects.filter(is_available=True)
+    products = Product.objects.filter(is_available=True).order_by('-id')[:6]
     context = {
         'products': products
     }
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.db.models import Q
 def store(request, category_slug=None):
    
     if category_slug == None:
@@ -11,13 +11,15 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(is_available=True, category=categories)
 
-    paginator = Paginator(products, 5)
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     paged_products = paginator.get_page(page)
     context = {
         'products': paged_products,
         'products_count': products.count()
     }
+    print("============")
+    
     return render(request, 'store.html', context)
 
 def product_detail(request, category_slug, product_slug):
